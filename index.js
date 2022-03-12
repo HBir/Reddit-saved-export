45404fa775b1aadaedc6c3e76718207ce67ed5ab
Hannes Birgersson <hannes@birgersson.dev>
2022-03-12 13:33:07 +0100

    change to synchronous download

--- index.js
+++ index.js
@@ -19,12 +19,12 @@ const r = new Snoowrap({
   password,
 });
 
-const linuxSafeString = (string) => string.replaceAll(/[^a-zA-Z_0-9åäö\\-]/ig, '_');
+const linuxSafeString = (string) => (string || "undefined").replaceAll(/[^a-zA-Z_0-9åäö\\-]/ig, '_');
 
 const getFileExtension = (string) => {
   // eslint-disable-next-line prefer-regex-literals
   const regexp = new RegExp('\\/[^\\/]*\\.([^\\.\\/]*)$');
-  return (string.match(regexp) || [])[1] || '';
+  return ((string || '').match(regexp) || [])[1] || '';
 };
 
 const markAsComplete = (url, filename) => {
@@ -35,8 +35,8 @@ const markAsComplete = (url, filename) => {
 const formatPostData = (post) => {
   const fileExtension = getFileExtension(post.url);
 
-  const filename = `${linuxSafeString(post.author.name)}_[`
-    + `${post.subreddit.display_name}]_`
+  const filename = `${linuxSafeString((post.author || {}).name)}_[`
+    + `${(post.subreddit || {}).display_name}]_`
     + `${linuxSafeString(post.title)}`
     + `.${fileExtension}`;
 
@@ -59,27 +59,43 @@ const downloadFile = async (url, folder, filename) => youtubedl(url, {
   });
 
 const handlePost = async (post) => {
-  if (filesDone[post.url]) {
-    console.log(`File already downloaded, skipping: ${post.url}`);
-    return '';
-  }
+  //if (filesDone[post.url]) {
+  //  console.log(`File already downloaded, skipping: ${post.url}`);
+  //  return '';
+  //}
 
   const postData = formatPostData(post);
-
-  await downloadFile(postData.url, postData.post_hint, postData.filename);
+  console.log('Downloading '+postData.url+" "+postData.filename);
+  //await downloadFile(postData.url, postData.post_hint, postData.filename);
 
   return postData;
 };
 
+const isAlreadyProcessed = (url) => {
+  if (filesDone[url]) { 
+    console.log(`File already downloaded, skipping: ${url}`);
+    return true;
+  }
+  return false;
+}
+
 const main = async () => {
+  console.log('Fetching content from Reddit...');
   const res = await r.getMe().getSavedContent()
     .fetchAll({ skipReplies: true })
     .filter((post) => !post.is_self)
-    .filter((post) => post.post_hint === 'image')
-    .map((post) => handlePost(post))
-    .filter((result) => result);
-
-  console.log(res);
+    .filter((post) => post.post_hint === 'rich:video')
+    .filter((post) => !isAlreadyProcessed(post.url))
+    .slice(0,10)
+  //.map((post) => handlePost(post))
+    //.filter((result) => result);
+  for (const post of res) {
+    await handlePost(post)
+  }
+  //console.log(res);
+  //res.map((data) => {
+  //  console.log(data.post_hint);
+  //});
   console.log(res.length);
 };
 
