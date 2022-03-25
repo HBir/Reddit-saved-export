ee2ad2fffc60689b6aa5237558b447469d5fef08
Hannes Birgersson <hannes@birgersson.dev>
2022-03-25 02:46:58 +0100

    doc comment

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ require('dotenv').config();
 
 const { exportSavedRedditPosts } = require('./src/post-handler');
 
+// The following environment variables can be used on `npm start`:
+// * `AMOUNT` Stop after processing this amount of posts
+// * `TYPE` Only process this type of `post_hint` posts (example: `image`, `rich:video` ...)
+// * `DRYRUN` Don't download any files
 const { AMOUNT, TYPE, DRYRUN } = process.env;
 
 const start = async () => exportSavedRedditPosts(TYPE, AMOUNT, DRYRUN);
